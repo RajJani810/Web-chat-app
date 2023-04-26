a60b42b6fd4b32f1b31c9a312c805ecfa0ca1890
RajJani810 <98929323+RajJani810@users.noreply.github.com>
2023-04-26 19:14:22 +0530

    Add files via upload
    add

---
+++ server.js
@@ -0,0 +1,36 @@
+
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+// const serverless = require('serverless-http');
+// const router = express.Router();
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
+})
+
+// app.use('/.netlify/functions/api', router);
+// module.exports=app;
+// module.exports.handler = serverless(app);
+
+
+
